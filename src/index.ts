87f1724f18de6d3bac87e1b9a7d48e00365ceab5
Argha Chandra Das <arghad@capitalnumbers.com>
2024-05-17 09:01:47 +0530

    add update and get restaurant

--- src/index.ts
+++ src/index.ts
@@ -3,8 +3,16 @@ import cors from 'cors';
 import 'dotenv/config';
 import mongoose from 'mongoose';
 import myUserRoute from './routes/myUserRoute';
+import myRestaurantRoute from './routes/myRestaurantRoute';
+import { v2 as cloudinary } from 'cloudinary';
 
 mongoose.connect(process.env.MONGODB_CONNECTION_STRING as string).then(() => console.log("Connected to database!"))
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET
+})
 
 const app = express();
 
@@ -17,6 +25,7 @@ app.get('/health', async (_req: Request, res: Response) => {
 })
 
 app.use('/api/my/user', myUserRoute)
+app.use('/api/my/restaurant', myRestaurantRoute)
 
 app.listen(9000, () => {
   console.log('Server started on port: 9000')
---
+++ src/routes/myRestaurantRoute.ts
@@ -0,0 +1,22 @@
+import express from 'express';
+import multer from 'multer';
+import myRestaurantController from '../controllers/myRestaurantController';
+import { jwtCheck, jwtParse } from '../middlewares/auth';
+import { validateMyRestaurantRequest } from '../middlewares/validation';
+
+const router = express.Router();
+
+const storage = multer.memoryStorage();
+const upload = multer({
+  storage: storage,
+  limits: {
+    fileSize: 5 * 1024 * 1024 // 5mb
+  }
+})
+
+router.get('/', jwtCheck, jwtParse, myRestaurantController.getMyRestaurant);
+router.post('/', upload.single('imageFile'), validateMyRestaurantRequest, jwtCheck, jwtParse, myRestaurantController.createMyRestaurant);
+
+router.put('/', upload.single('imageFile'), validateMyRestaurantRequest, jwtCheck, jwtParse, myRestaurantController.updateMyRestaurant);
+
+export default router;
